5415efe9f274d4b0a676883f71d5148ae01df381
EASummer <42003473+EASummer@users.noreply.github.com>
2018-08-09 13:53:40 -0700

    added zoom feature

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ def print_state(race):
     for i in range (0,mazy.xLen):
         for j in range(0,mazy.yLen):
             if mazy.grid[i][j]== 1:
-                img.put("#000000", (i+3, j+3))
+                img.put("#000000", (i+2, j+2))
             if mazy.grid[i][j]== 0:
-                img.put("#ffffff", (i+3, j+3))
+                img.put("#ffffff", (i+2, j+2))
 
 def text_print(race):
     mazy = race.mazy
@@ -22,17 +22,22 @@ def text_print(race):
 
 race = Race()
 
-width = race.mazy.xLen
-height = race.mazy.yLen
+width = race.mazy.xLen+2
+height = race.mazy.yLen+2
 
 window = Tk()
 canvas = Canvas(window, width=width, height=height, bg="#000000")
-canvas.pack()
 img = PhotoImage(width=width, height=height)
+print_state(race)
 
 
 canvas.create_image((width // 2, height // 2), image=img, state="normal")
-print_state(race)
 
+#make image larger
+zoomAmount= 10
+canvas = Canvas(window, width=width*zoomAmount, height=height*zoomAmount, bg="#000000")
+canvas.pack()
+img = img.zoom(10,10)
+canvas.create_image((width*zoomAmount // 2, height*zoomAmount // 2), image=img, state="normal")
 
 mainloop()
